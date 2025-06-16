b49f5c205b96210992947401b7f2fc7f6d856df5
truple98 <ljw122321@gmail.com>
2025-06-16 11:09:13 +0900

    modify

--- deploy.sh
+++ deploy.sh
@@ -25,10 +25,12 @@ else
     cat << EOF > .env
 OPENAI_API_KEY=${OPENAI_API_KEY}
 TAVILY_API_KEY=${TAVILY_API_KEY}
-EOF # 환경 변수 설정
+EOF
+# 환경 변수 설정
     sudo chown ubuntu:ubuntu .env
     echo "New .env file created"
-fi # .env 파일 생성
+fi
+# .env 파일 생성
 
 # .env 파일 확인
 echo "Checking .env file..."
